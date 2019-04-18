8ddcdddf253c7aaa6a233e8174f291414185a139
Martinho <osvgomes@terra.com.br>
2019-04-18 07:57:04 -0300

    add changes to data input and vocab

--- twitterbot_ai.py
+++ twitterbot_ai.py
@@ -5,6 +5,11 @@ from sample import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-thistext = main()
+
+thistext = ''
+
+while thistext == '' or len(thistext) > 280:
+    thistext = main()
+
 api.update_status(thistext)
 print (thistext)
